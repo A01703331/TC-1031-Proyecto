8f281d8b3de42cdfce29ee9afbd421f5ac8bffda
A01703331 <69860747+A01703331@users.noreply.github.com>
2021-11-29 11:59:24 -0600

    Correcion de compilacion
    El programa ahora compila exitosamente

--- filer.h
+++ filer.h
@@ -12,43 +12,127 @@
 #include <sstream>
 #include <math.h>
 
-template <class T> class BSTree;
+template <class T1, class T2, class T3> class BSTree;
 
-template <class T>
+template <class T1, class T2, class T3>
 class Run {
 private:
-    Run();
-	Run(std::string, unsigned int, std::string);
-	Run(std::string, unsigned int, std::string, Run<T>*, Run<T>*);
-	Run(const Run<T>&);
     unsigned int Tseconds, hours, minutes,seconds;
     std::string name, version;
-	Run<T>* add(std::string, int, std::string);//
-	Run<T> *faster, *slower, *previous, *next;
-    void TotalTime(unsigned int, unsigned int, unsigned int);
-	void removeChilds(); //
-	void fromFastest(std::stringstream&) const;//
-    int maxHeight(Run<T>*);//
     
 public:
-    std::string printTime() const;
-	friend class BSTree<T>;
+	Run(std::string, unsigned int, std::string);
+	Run(std::string, unsigned int, std::string, Run<T1,T2,T3>*, Run<T1,T2,T3>*);
+	Run(const Run<T1,T2,T3>&);
+	Run<T1,T2,T3> *faster, *slower, *previous, *next;
+    void TotalTime(unsigned int, unsigned int, unsigned int);
+	Run<T1,T2,T3>* add(std::string, unsigned int, std::string);
+	Run<T1,T2,T3>* succesor();
+	void remove(std::string, unsigned int, std::string);
+	void removeChilds(); //
+	void fromFastest(std::stringstream&);//
+	void byName(std::stringstream&, std::string);
+    int maxHeight(Run<T1,T2,T3>*);//
+    std::string printTime();
+	friend class BSTree<T1,T2,T3>;
 };
 
-template <class T> //Constructor por default
-Run<T>::Run(): name(""), Tseconds(0), version(""), slower(NULL), faster(NULL){
+template <class T1, class T2, class T3> //Constructor simple
+Run<T1,T2,T3>::Run(std::string nm, unsigned int secs, std::string ver): name(nm), Tseconds(secs), version(ver), slower(NULL), faster(NULL){
 }
 
-template <class T> //Constructor simple
-Run<T>::Run(std::string nm, unsigned int secs, std::string ver): name(nm), Tseconds(secs), version(ver), slower(NULL), faster(NULL){
+template <class T1, class T2, class T3> //Constructor con nodos
+Run<T1,T2,T3>::Run(std::string nm, unsigned int secs, std::string ver, Run *slw, Run *fst) : name(nm), Tseconds(secs), version(ver), slower(slw), faster(fst){
 }
 
-template <class T> //Constructor con nodos
-Run<T>::Run(std::string nm, unsigned int secs, std::string ver, Run *slw, Run *fst) : name(nm), Tseconds(secs), version(ver), slower(slw), faster(fst){
+template <class T1, class T2, class T3> //Agrega un nuevo Run al arbol
+Run<T1,T2,T3>* Run<T1,T2,T3>::add(std::string nm, unsigned int secs, std::string ver) {
+	if (secs < Tseconds) {
+		if (faster != NULL) {
+			return faster->add(nm, secs, ver);
+		} else {
+			return faster = new Run<T1,T2,T3>(nm, secs, ver);
+		}
+	} else {
+		if (slower != NULL) {
+			return slower->add(nm, secs, ver);
+		} else {
+			return slower = new Run<T1,T2,T3>(nm, secs, ver);
+		}
+	}
 }
 
-template <class T>
-void Run<T>::removeChilds() {
+
+template <class T1, class T2, class T3> 
+Run<T1,T2,T3>* Run<T1,T2,T3>::succesor() {
+	Run<T1,T2,T3> *fs, *sl;
+
+	fs = faster;
+	sl = slower;
+
+	if (faster == NULL) {
+		if (slower != NULL) {
+			slower = NULL;
+		}
+		return sl;
+	}
+
+	if (faster->slower == NULL) {
+		faster = faster->faster;
+		fs->faster = NULL;
+		return fs;
+	}
+
+	Run<T1,T2,T3> *parent, *child;
+	parent = faster;
+	child = faster->slower;
+	while (child->slower != NULL) {
+		parent = child;
+		child = child->slower;
+	}
+	parent->slower = child->faster;
+	child->faster = NULL;
+	return child;
+}
+
+template <class T1, class T2, class T3> 
+void Run<T1,T2,T3>::remove(std::string nm, unsigned int secs, std::string ver) {
+	Run<T1,T2,T3> * succ, *old;
+	if (secs < Tseconds) {
+		if (faster != NULL) {
+			if (faster->Tseconds == secs) {
+				old = faster;
+				succ = faster->succesor();
+				if (succ != NULL) {
+					succ->faster = old->faster;
+					succ->slower = old->slower;
+				}
+				faster = succ;
+				delete old;
+			} else {
+				faster->remove(nm, secs, ver);
+			}
+		}
+	} else if (secs > Tseconds) {
+		if (slower != NULL) {
+			if (slower->Tseconds == secs) {
+				old = slower;
+				succ = slower->succesor();
+				if (succ != NULL) {
+					succ->faster = old->faster;
+					succ->slower = old->slower;
+				}
+				slower = succ;
+				delete old;
+			} else {
+				slower->remove(nm, secs, ver);
+			}
+		}
+	}
+}
+
+template <class T1, class T2, class T3>
+void Run<T1,T2,T3>::removeChilds() {
 	if (faster != NULL) {
 		faster->removeChilds();
 		delete faster;
@@ -61,30 +145,13 @@ void Run<T>::removeChilds() {
 	}
 }
 
-template <class T> //Agrega un nuevo Run al arbol
-Run<T>* Run<T>::add(std::string nm, int secs, std::string ver) {
-	if (secs < Tseconds) {
-		if (faster != NULL) {
-			return faster->add(nm, secs, ver);
-		} else {
-			return faster = new Run<T>(nm, secs, ver);
-		}
-	} else {
-		if (slower != NULL) {
-			return slower->add(nm, secs, ver);
-		} else {
-			return slower = new Run<T>(nm, secs, ver);
-		}
-	}
-}
-
-template <class T>
-void Run<T>::TotalTime(unsigned int hrs, unsigned int mins, unsigned int secs) {
+template <class T1, class T2, class T3>
+void Run<T1,T2,T3>::TotalTime(unsigned int hrs, unsigned int mins, unsigned int secs) {
     Tseconds = hrs*3600 + mins*60 + secs;
 }
 
-template <class T>
-std::string Run<T>::printTime() const {
+template <class T1, class T2, class T3>
+std::string Run<T1,T2,T3>::printTime() {
 	std::stringstream aux;
     seconds = Tseconds;
     minutes = floor(Tseconds/60);
@@ -93,13 +160,19 @@ std::string Run<T>::printTime() const {
     if (hours>0){
         aux << hours << ":";
     }
+	if (minutes%60 < 10){
+		aux << "0";
+	}
     aux << minutes%60 << ":";
+	if (seconds%60 < 10){
+		aux << "0";
+	}
     aux << seconds%60;
     return aux.str();
 }
 
-template <class T>
-int Run<T>::maxHeight(Run<T> *segment){
+template <class T1, class T2, class T3>
+int Run<T1,T2,T3>::maxHeight(Run<T1,T2,T3> *segment){
 	if (segment!=NULL){
 		int L = maxHeight(segment->faster);
 		int R = maxHeight(segment->slower);
@@ -113,80 +186,125 @@ int Run<T>::maxHeight(Run<T> *segment){
 	}
 }
 
-template <class T>
-void Run<T>::fromFastest(std::stringstream &aux) const {
+template <class T1, class T2, class T3>
+void Run<T1,T2,T3>::fromFastest(std::stringstream &aux) {
     if (faster != NULL) {
 	    faster->fromFastest(aux);
-	    aux << " ";
+	    aux << "\n";
 	}
     aux << name << ",";
 	aux << printTime() << ",";
     aux << version;
 	if (slower != NULL) {
-        aux << " ";
+        aux << "\n";
 	    slower->fromFastest(aux);
     }
 }
 
-template <class T>
+template <class T1, class T2, class T3>
+void Run<T1,T2,T3>::byName(std::stringstream &aux, std::string nm) {
+	if (faster != NULL) {
+	    faster->byName(aux, nm);
+	}
+	if (nm == name) {
+    	aux << name << ",";
+		aux << printTime() << ",";
+    	aux << version;
+	    aux << "\n";
+	}
+	if (slower != NULL) {
+	    slower->byName(aux, nm);
+    }
+}
+
+template <class T1, class T2, class T3>
 class BSTree {
 private:
-	Run<T> *root, *head, *tail;
+	Run<T1,T2,T3> *root, *head, *tail;
     unsigned int tamanio;
 
 public:
-	BSTree();
-	~BSTree();
-	bool empty() const;
+	BSTree();//
+	~BSTree();//
+	bool empty() const;//
 	void add(std::string, int, std::string); //
 	void addWR(std::string, int, std::string); //
-	void remove(int);
-	void removeAll();
-	std::string fromFastest() const; //
-    int size() const; //
-	int height() const; //
+	void remove(std::string, int, std::string);
+	void removeAll();//
+	std::string fromFastest(); //
+    std::string byName(std::string); //
+	int size() const; //
+	int height(); //
 };
 
-template <class T> //Verifica si el arbol y la lista estan vacios
-bool BSTree<T>::empty() const{
-    return (head==NULL && root==NULL);
+template <class T1, class T2, class T3>
+BSTree<T1,T2,T3>::BSTree() :head(0), tail(0), root(0), tamanio(0) {
 }
 
-template <class T> //Agrega un nuevo Run al arbol y al final de la lista vinculada
-void BSTree<T>::add(std::string nm, int secs, std::string ver){
-    if (!empty){
-        Run<T> *addition = root->add(nm,secs,ver);
+template <class T1, class T2, class T3>
+BSTree<T1,T2,T3>::~BSTree() {
+	removeAll();
+}
+
+template <class T1, class T2, class T3> //Verifica si el arbol y la lista estan vacios
+bool BSTree<T1,T2,T3>::empty() const{
+    return ((head==NULL && tail==NULL) && tail==NULL);
+}
+
+template <class T1, class T2, class T3> //Agrega un nuevo Run al arbol y al final de la lista vinculada
+void BSTree<T1,T2,T3>::add(std::string nm, int secs, std::string ver){
+    if (!empty()){
+        Run<T1,T2,T3> *addition = root->add(nm,secs,ver);
 	    tail->next = addition;
 	    addition->previous = tail;
 	    tail = addition;
         tamanio++;
     } else {
-        root = new Run<T>(nm, secs, ver);
+        root = new Run<T1,T2,T3>(nm, secs, ver);
         head = root;
         tail = root;
         tamanio++;
     }
 }
 
-template <class T> //Agrega un nuevo Run al arbol y al principio de la lista vinculada
-void BSTree<T>::addWR(std::string nm, int secs, std::string ver){
-    if (!empty){
-        Run<T> *wr = root->add(nm,secs,ver);
+template <class T1, class T2, class T3> //Agrega un nuevo Run al arbol y al principio de la lista vinculada
+void BSTree<T1,T2,T3>::addWR(std::string nm, int secs, std::string ver){
+    if (!empty()){
+        Run<T1,T2,T3> *wr = root->add(nm,secs,ver);
 	    wr->next = head;
         head->previous = wr;
     	head = wr;
 	    tamanio++;
     } else {
-        root = new Run<T>(nm, secs, ver);
+        root = new Run<T1,T2,T3>(nm, secs, ver);
         head = root;
         tail = root;
         tamanio++;
     }
 }
 
-template <class T>
-void BSTree<T>::removeAll() {
-    Run<T> *p, *q;
+template <class T1, class T2, class T3>
+void BSTree<T1,T2,T3>::remove(std::string nm, int secs, std::string ver) {
+	if (root != NULL) {
+		if (nm == root->name && secs == root->Tseconds && ver == root->version) {
+			Run<T1,T2,T3> *succ = root->succesor();
+			if (succ != NULL) {
+				succ->faster = root->faster;
+				succ->slower = root->slower;
+			}
+			delete root;
+			root = succ;
+			tamanio--;
+		} else {
+			root->remove(nm, secs, ver);
+			tamanio--;
+		}
+	}
+}
+
+template <class T1, class T2, class T3>
+void BSTree<T1,T2,T3>::removeAll() {
+    Run<T1,T2,T3> *p, *q;
 
 	p = head;
 	while (p != NULL){
@@ -204,15 +322,22 @@ void BSTree<T>::removeAll() {
     tamanio = 0;
 }
 
-template <class T>
-std::string BSTree<T>::fromFastest() const{
+template <class T1, class T2, class T3> // Imprime tiempos desde el mas rapido
+std::string BSTree<T1,T2,T3>::fromFastest(){
     std::stringstream aux;
-    aux << root->fromFastest(aux, 10);
+	root->fromFastest(aux);
     return aux.str();
 }
 
-template <class T>  // Regresa la altura del arbol
-int BSTree<T>::height() const{
+template <class T1, class T2, class T3> // Imprime tiempos de una persona en especifico	
+std::string BSTree<T1,T2,T3>::byName(std::string nm){
+    std::stringstream aux;
+	root->byName(aux, nm);
+    return aux.str();
+}
+
+template <class T1, class T2, class T3>  // Regresa la altura del arbol
+int BSTree<T1,T2,T3>::height(){
 	if (!empty()){
 		return root->maxHeight(root);
 	} else {
@@ -220,8 +345,8 @@ int BSTree<T>::height() const{
 	}
 }
 
-template <class T> //Regresa el tamanio de la lista vinculada
-int BSTree<T>::size() const{
+template <class T1, class T2, class T3> //Regresa el tamanio de la lista vinculada
+int BSTree<T1,T2,T3>::size() const{
     return tamanio;
 }
 
--- main.cpp
+++ main.cpp
@@ -15,11 +15,48 @@
 
 using namespace std;
 
-int main() {
+int main(int argc, char* argv[]) {
+    BSTree<std::string, int, std::string> Tanooki;
     std::ofstream Runs("SMO-test.csv"); // Toma un archivo de salida
-    Runs << "Welcome New Galaxy!";
+    Runs << "Name,Time,Version" << "\n";
     
-    Runs.close(); // Cierra el archivo
+
+    if (Tanooki.empty()){  
+        cout << "El arbol y la lista estan vacios" << "\n";
+    } else {
+        cout << "El arbol y la lista no estan vacios" << "\n";
+    }
     
+    Tanooki.add("ML",4381,"1.3.0");
+    Tanooki.add("Trop",5012,"1.2.0");
+    Tanooki.add("Ren",8429,"1.0.0");
+    Tanooki.addWR("ML",4294,"1.3.0");
+    Tanooki.addWR("Goro",4203,"1.3.1");
+    Tanooki.add("ML",4223,"1.3.0");
+    Tanooki.add("Tari",5231,"1.0.0");
+    Tanooki.add("Saber",4932,"1.2.0");
+    Tanooki.add("NecoArc",4423,"1.2.0");
+    Tanooki.addWR("Pringle",3523,"1.2.0");
+    Tanooki.addWR("ML",3487,"1.3.0");
+    cout << Tanooki.fromFastest() << "\n";
+    cout << "La lista tiene " << Tanooki.size() << " nodos.\n";
+    cout << "El arbol tiene una altura de " << Tanooki.height() << ".\n";
+ 
+    if (Tanooki.empty()){  
+        cout << "El arbol y la lista estan vacios" << "\n";
+    } else {
+        cout << "El arbol y la lista no estan vacios" << "\n";
+    }
+
+    Tanooki.remove("NecoArc",4423,"1.2.0");
+    cout << Tanooki.fromFastest() << "\n";
+    cout << "La lista tiene " << Tanooki.size() << " nodos.\n";
+    cout << "El arbol tiene una altura de " << Tanooki.height() << ".\n";   
+
+    cout << "Los Runs de ML son:\n" << Tanooki.byName("ML");
+    
+    Runs << Tanooki.fromFastest() << "\n";
+    Runs.close();
+
     return 0;
 }
