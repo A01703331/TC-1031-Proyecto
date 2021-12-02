fa1a111dd20d65a9e2a37b7b7ca13a1518568bc3
A01703331 <69860747+A01703331@users.noreply.github.com>
2021-12-02 12:23:40 -0600

    Add files via upload

--- filer.h
+++ filer.h
@@ -15,9 +15,9 @@
 #include <math.h>
 #include <algorithm>
 
-template <class T1, class T2, class T3> class BSTree;
+template <class Player, class Time, class Version> class BSTree;
 
-template <class T1, class T2, class T3>
+template <class Player, class Time, class Version>
 class Run {
 private:
     unsigned int Tseconds, hours, minutes, seconds;
@@ -25,51 +25,52 @@ private:
     
 public:
 	Run(std::string, unsigned int, std::string);
-	Run(std::string, unsigned int, std::string, Run<T1,T2,T3>*, Run<T1,T2,T3>*);
-	Run(const Run<T1,T2,T3>&);
-	Run<T1,T2,T3> *faster, *slower, *next;
+	Run(std::string, unsigned int, std::string, Run<Player,Time,Version>*, Run<Player,Time,Version>*);
+	Run(const Run<Player,Time,Version>&);
+	Run<Player,Time,Version> *faster, *slower, *next, *previous;
     void TotalTime(unsigned int, unsigned int, unsigned int);
-	Run<T1,T2,T3>* add(std::string, unsigned int, std::string);
-	Run<T1,T2,T3>* succesor();
-	void remove(std::string, unsigned int, std::string, std::vector<Run<T1,T2,T3>*>&);
+	Run<Player,Time,Version>* add(std::string, unsigned int, std::string);
+	Run<Player,Time,Version>* succesor();
+	void remove(std::string, unsigned int, std::string, std::vector<Run<Player,Time,Version>*>&);
 	void removeChilds(); //
+	void removeLinks();
 	void fromFastest(std::stringstream&);//
 	void byName(std::stringstream&, std::string);
-    int maxHeight(Run<T1,T2,T3>*);//
+    int maxHeight(Run<Player,Time,Version>*);//
     std::string printTime();//
 	void printLinkList(std::stringstream&);
-	friend class BSTree<T1,T2,T3>;
+	friend class BSTree<Player,Time,Version>;
 };
 
-template <class T1, class T2, class T3> //Constructor simple
-Run<T1,T2,T3>::Run(std::string nm, unsigned int secs, std::string ver): name(nm), Tseconds(secs), version(ver), slower(NULL), faster(NULL){
+template <class Player, class Time, class Version> //Constructor simple
+Run<Player,Time,Version>::Run(std::string nm, unsigned int secs, std::string ver): name(nm), Tseconds(secs), version(ver), slower(NULL), faster(NULL){
 }
 
-template <class T1, class T2, class T3> //Constructor con nodos
-Run<T1,T2,T3>::Run(std::string nm, unsigned int secs, std::string ver, Run *slw, Run *fst) : name(nm), Tseconds(secs), version(ver), slower(slw), faster(fst){
+template <class Player, class Time, class Version> //Constructor con nodos
+Run<Player,Time,Version>::Run(std::string nm, unsigned int secs, std::string ver, Run *slw, Run *fst) : name(nm), Tseconds(secs), version(ver), slower(slw), faster(fst){
 }
 
-template <class T1, class T2, class T3> //Agrega un nuevo Run al arbol
-Run<T1,T2,T3>* Run<T1,T2,T3>::add(std::string nm, unsigned int secs, std::string ver) {
+template <class Player, class Time, class Version> //Agrega un nuevo Run al arbol
+Run<Player,Time,Version>* Run<Player,Time,Version>::add(std::string nm, unsigned int secs, std::string ver) {
 	if (secs < Tseconds) {
 		if (faster != NULL) {
 			return faster->add(nm, secs, ver);
 		} else {
-			return faster = new Run<T1,T2,T3>(nm, secs, ver);
+			return faster = new Run<Player,Time,Version>(nm, secs, ver);
 		}
 	} else {
 		if (slower != NULL) {
 			return slower->add(nm, secs, ver);
 		} else {
-			return slower = new Run<T1,T2,T3>(nm, secs, ver);
+			return slower = new Run<Player,Time,Version>(nm, secs, ver);
 		}
 	}
 }
 
 
-template <class T1, class T2, class T3> 
-Run<T1,T2,T3>* Run<T1,T2,T3>::succesor() {
-	Run<T1,T2,T3> *fs, *sl;
+template <class Player, class Time, class Version> 
+Run<Player,Time,Version>* Run<Player,Time,Version>::succesor() {
+	Run<Player,Time,Version> *fs, *sl;
 
 	fs = faster;
 	sl = slower;
@@ -87,7 +88,7 @@ Run<T1,T2,T3>* Run<T1,T2,T3>::succesor() {
 		return fs;
 	}
 
-	Run<T1,T2,T3> *parent, *child;
+	Run<Player,Time,Version> *parent, *child;
 	parent = faster;
 	child = faster->slower;
 	while (child->slower != NULL) {
@@ -99,9 +100,9 @@ Run<T1,T2,T3>* Run<T1,T2,T3>::succesor() {
 	return child;
 }
 
-template <class T1, class T2, class T3> 
-void Run<T1,T2,T3>::remove(std::string nm, unsigned int secs, std::string ver, std::vector<Run<T1,T2,T3>*>&TimeList) {
-	Run<T1,T2,T3> * succ, *old;
+template <class Player, class Time, class Version> 
+void Run<Player,Time,Version>::remove(std::string nm, unsigned int secs, std::string ver, std::vector<Run<Player,Time,Version>*>&TimeList) {
+	Run<Player,Time,Version> * succ, *old;
 	if (secs < Tseconds) {
 		if (faster != NULL) {
 			if (faster->Tseconds == secs) {
@@ -112,8 +113,8 @@ void Run<T1,T2,T3>::remove(std::string nm, unsigned int secs, std::string ver, s
 					succ->slower = old->slower;
 				}
 				faster = succ;
-				TimeList.erase(std::remove(TimeList.begin(), TimeList.end(), old), TimeList.end());
 				delete old;
+				TimeList.erase(std::remove(TimeList.begin(), TimeList.end(), old), TimeList.end());
 			} else {
 				faster->remove(nm, secs, ver, TimeList);
 			}
@@ -128,8 +129,8 @@ void Run<T1,T2,T3>::remove(std::string nm, unsigned int secs, std::string ver, s
 					succ->slower = old->slower;
 				}
 				slower = succ;
-				TimeList.erase(std::remove(TimeList.begin(), TimeList.end(), old), TimeList.end());
 				delete old;
+				TimeList.erase(std::remove(TimeList.begin(), TimeList.end(), old), TimeList.end());
 			} else {
 				slower->remove(nm, secs, ver, TimeList);
 			}
@@ -137,8 +138,8 @@ void Run<T1,T2,T3>::remove(std::string nm, unsigned int secs, std::string ver, s
 	}
 }
 
-template <class T1, class T2, class T3>
-void Run<T1,T2,T3>::removeChilds() {
+template <class Player, class Time, class Version>
+void Run<Player,Time,Version>::removeChilds() {
 	if (faster != NULL) {
 		faster->removeChilds();
 		delete faster;
@@ -149,20 +150,27 @@ void Run<T1,T2,T3>::removeChilds() {
 		delete slower;
 		slower = NULL;
 	}
-	if (next != NULL) {
-		next->removeChilds();
-		delete next;
-		next = NULL;
-	}
 }
 
-template <class T1, class T2, class T3>
-void Run<T1,T2,T3>::TotalTime(unsigned int hrs, unsigned int mins, unsigned int secs) {
+template <class Player, class Time, class Version>
+void Run<Player,Time,Version>::removeLinks() {
+	if (next!=NULL){
+		if (previous!=NULL){
+			previous = NULL;
+		}
+		next->removeLinks();
+		return;
+	}
+	previous = NULL;
+}
+
+template <class Player, class Time, class Version>
+void Run<Player,Time,Version>::TotalTime(unsigned int hrs, unsigned int mins, unsigned int secs) {
     Tseconds = hrs*3600 + mins*60 + secs;
 }
 
-template <class T1, class T2, class T3>
-void Run<T1,T2,T3>::fromFastest(std::stringstream &aux) {
+template <class Player, class Time, class Version>
+void Run<Player,Time,Version>::fromFastest(std::stringstream &aux) {
     if (faster != NULL) {
 	    faster->fromFastest(aux);
 	    aux << "\n";
@@ -176,8 +184,8 @@ void Run<T1,T2,T3>::fromFastest(std::stringstream &aux) {
     }
 }
 
-template <class T1, class T2, class T3>
-void Run<T1,T2,T3>::byName(std::stringstream &aux, std::string nm) {
+template <class Player, class Time, class Version>
+void Run<Player,Time,Version>::byName(std::stringstream &aux, std::string nm) {
     if (faster != NULL) {
 	    faster->byName(aux, nm);
 	}
@@ -192,8 +200,8 @@ void Run<T1,T2,T3>::byName(std::stringstream &aux, std::string nm) {
     }
 }
 
-template <class T1, class T2, class T3>
-int Run<T1,T2,T3>::maxHeight(Run<T1,T2,T3> *segment){
+template <class Player, class Time, class Version>
+int Run<Player,Time,Version>::maxHeight(Run<Player,Time,Version> *segment){
 	if (segment!=NULL){
 		int L = maxHeight(segment->faster);
 		int R = maxHeight(segment->slower);
@@ -207,8 +215,8 @@ int Run<T1,T2,T3>::maxHeight(Run<T1,T2,T3> *segment){
 	}
 }
 
-template <class T1, class T2, class T3>
-std::string Run<T1,T2,T3>::printTime() {
+template <class Player, class Time, class Version>
+std::string Run<Player,Time,Version>::printTime() {
 	std::stringstream aux;
     seconds = Tseconds;
     minutes = floor(Tseconds/60);
@@ -228,8 +236,8 @@ std::string Run<T1,T2,T3>::printTime() {
     return aux.str();
 }
 
-template <class T1, class T2, class T3>
-void Run<T1,T2,T3>::printLinkList(std::stringstream &aux) {
+template <class Player, class Time, class Version>
+void Run<Player,Time,Version>::printLinkList(std::stringstream &aux) {
     aux << name << ",";
 	aux << printTime() << ",";
     aux << version;
@@ -239,15 +247,15 @@ void Run<T1,T2,T3>::printLinkList(std::stringstream &aux) {
 	}
 }
 
-template <class T1, class T2, class T3>
+template <class Player, class Time, class Version>
 class BSTree {
 private:
-	std::vector<Run<T1,T2,T3>*> TimeList;
-	Run<T1,T2,T3> *root, *head, *tail;
+	std::vector<Run<Player,Time,Version>*> TimeList;
+	Run<Player,Time,Version> *root, *head, *tail;
     unsigned int tamanio;
-	void mergeSplit(std::vector<Run<T1,T2,T3>*>&, int, int);
-	void mergeArr(std::vector<Run<T1,T2,T3>*>&, int, int, int);
-	void copyArr(std::vector<Run<T1,T2,T3>*>&, int, int);
+	void mergeSplit(std::vector<Run<Player,Time,Version>*>&, int, int);
+	void mergeArr(std::vector<Run<Player,Time,Version>*>&, int, int, int);
+	void copyArr(std::vector<Run<Player,Time,Version>*>&, int, int);
 
 public:
 	BSTree();//
@@ -265,28 +273,28 @@ public:
 	std::string printLinkList();
 };
 
-template <class T1, class T2, class T3>
-BSTree<T1,T2,T3>::BSTree() :root(0), tamanio(0) {
+template <class Player, class Time, class Version>
+BSTree<Player,Time,Version>::BSTree() :root(0), tamanio(0) {
 }
 
-template <class T1, class T2, class T3>
-BSTree<T1,T2,T3>::~BSTree() {
+template <class Player, class Time, class Version>
+BSTree<Player,Time,Version>::~BSTree() {
 	removeAll();
 }
 
-template <class T1, class T2, class T3> //Verifica si el arbol y la lista estan vacios
-bool BSTree<T1,T2,T3>::empty() const{
+template <class Player, class Time, class Version> //Verifica si el arbol y la lista estan vacios
+bool BSTree<Player,Time,Version>::empty() const{
     return (root==NULL);
 }
 
-template <class T1, class T2, class T3> //Agrega un nuevo Run al arbol y al final de la lista vinculada
-void BSTree<T1,T2,T3>::add(std::string nm, int secs, std::string ver){
+template <class Player, class Time, class Version> //Agrega un nuevo Run al arbol y al final de la lista vinculada
+void BSTree<Player,Time,Version>::add(std::string nm, int secs, std::string ver){
     if (!empty()){
-        Run<T1,T2,T3> *addition = root->add(nm,secs,ver);
+        Run<Player,Time,Version> *addition = root->add(nm,secs,ver);
 		TimeList.push_back(addition);
         tamanio++;
     } else {
-        root = new Run<T1,T2,T3>(nm, secs, ver);
+        root = new Run<Player,Time,Version>(nm, secs, ver);
 		head = root;
 		tail = root;
 		TimeList.push_back(root);
@@ -294,11 +302,11 @@ void BSTree<T1,T2,T3>::add(std::string nm, int secs, std::string ver){
     }
 }
 
-template <class T1, class T2, class T3>
-void BSTree<T1,T2,T3>::remove(std::string nm, int secs, std::string ver) {
+template <class Player, class Time, class Version>
+void BSTree<Player,Time,Version>::remove(std::string nm, int secs, std::string ver) {
 	if (root != NULL) {
 		if (nm == root->name && secs == root->Tseconds && ver == root->version) {
-			Run<T1,T2,T3> *succ = root->succesor();
+			Run<Player,Time,Version> *succ = root->succesor();
 			if (succ != NULL) {
 				succ->faster = root->faster;
 				succ->slower = root->slower;
@@ -314,8 +322,8 @@ void BSTree<T1,T2,T3>::remove(std::string nm, int secs, std::string ver) {
 	}
 }
 
-template <class T1, class T2, class T3>
-void BSTree<T1,T2,T3>::removeAll() {
+template <class Player, class Time, class Version>
+void BSTree<Player,Time,Version>::removeAll() {
 	if (root != NULL) {
 		root->removeChilds();
 	}
@@ -327,22 +335,22 @@ void BSTree<T1,T2,T3>::removeAll() {
     tamanio = 0;
 }
 
-template <class T1, class T2, class T3> // Imprime tiempos desde el mas rapido
-std::string BSTree<T1,T2,T3>::fromFastest(){
+template <class Player, class Time, class Version> // Imprime tiempos desde el mas rapido
+std::string BSTree<Player,Time,Version>::fromFastest(){
     std::stringstream aux;
 	root->fromFastest(aux);
     return aux.str();
 }
 
-template <class T1, class T2, class T3> // Imprime tiempos de una persona en especifico	
-std::string BSTree<T1,T2,T3>::byName(std::string nm){
+template <class Player, class Time, class Version> // Imprime tiempos de una persona en especifico	
+std::string BSTree<Player,Time,Version>::byName(std::string nm){
     std::stringstream aux;
 	root->byName(aux, nm);
     return aux.str();
 }
 
-template <class T1, class T2, class T3>  // Regresa la altura del arbol
-int BSTree<T1,T2,T3>::height(){
+template <class Player, class Time, class Version>  // Regresa la altura del arbol
+int BSTree<Player,Time,Version>::height(){
 	if (!empty()){
 		return root->maxHeight(root);
 	} else {
@@ -350,23 +358,22 @@ int BSTree<T1,T2,T3>::height(){
 	}
 }
 
-template <class T1, class T2, class T3> //Regresa el tamanio de la lista vinculada
-int BSTree<T1,T2,T3>::size() const{
+template <class Player, class Time, class Version> //Regresa el tamanio de la lista vinculada
+int BSTree<Player,Time,Version>::size() const{
     return tamanio;
 }
 
-
-template <class T1, class T2, class T3>
-void BSTree<T1,T2,T3>::copyArr(std::vector<Run<T1,T2,T3>*> &B, int L, int H) {
+template <class Player, class Time, class Version>
+void BSTree<Player,Time,Version>::copyArr(std::vector<Run<Player,Time,Version>*> &B, int L, int H) {
 	for (int i = L; i <= H; i++) {
 		TimeList[i] = B[i];
 	};
 };
 
 
-template <class T1, class T2, class T3>
-void BSTree<T1,T2,T3>::mergeArr(std::vector<Run<T1,T2,T3>*> &B, int L, int hal, int H) {
-	size_t i, j, k;
+template <class Player, class Time, class Version>
+void BSTree<Player,Time,Version>::mergeArr(std::vector<Run<Player,Time,Version>*> &B, int L, int hal, int H) {
+	int i, j, k;
 
 	i = L;
 	j = hal + 1;
@@ -392,8 +399,8 @@ void BSTree<T1,T2,T3>::mergeArr(std::vector<Run<T1,T2,T3>*> &B, int L, int hal, 
 	};
 };
 
-template <class T1, class T2, class T3>
-void BSTree<T1,T2,T3>::mergeSplit(std::vector<Run<T1,T2,T3>*> &B, int L, int H) {
+template <class Player, class Time, class Version>
+void BSTree<Player,Time,Version>::mergeSplit(std::vector<Run<Player,Time,Version>*> &B, int L, int H) {
 	if ((H - L) < 1) {
 		return; // Verifica si ya no se puede separar en mitades
 	}
@@ -404,32 +411,34 @@ void BSTree<T1,T2,T3>::mergeSplit(std::vector<Run<T1,T2,T3>*> &B, int L, int H) 
 	copyArr(B, L, H); // Copia los valores de B hacia TimeList
 };
 
-template <class T1, class T2, class T3>
-void BSTree<T1,T2,T3>::sortMerge(){
-    std::vector<Run<T1,T2,T3>*> tarr(tamanio);
+template <class Player, class Time, class Version>
+void BSTree<Player,Time,Version>::sortMerge(){
+    std::vector<Run<Player,Time,Version>*> tarr(tamanio);
     mergeSplit(tarr, 0, (tamanio - 1));
 };
 
-
-template <class T1, class T2, class T3>
-void BSTree<T1,T2,T3>::linkTimes(){
+template <class Player, class Time, class Version>
+void BSTree<Player,Time,Version>::linkTimes(){
 	head = TimeList[0];
 	tail = TimeList[tamanio-1];
+	head->previous = NULL;
 	for (int i=0;i<tamanio;i++){
-		TimeList[i]->next=TimeList[i+1];
 		if (i==tamanio-1) {
 			TimeList[i]->next=NULL;
+			return;
 		}
+		TimeList[i]->next=TimeList[i+1];
+		TimeList[i+1]->previous=TimeList[i];
+		
 	}
 }
 
-template <class T1, class T2, class T3> // Imprime tiempos desde el mas rapido
-std::string BSTree<T1,T2,T3>::printLinkList(){
+template <class Player, class Time, class Version> // Imprime tiempos desde el mas rapido
+std::string BSTree<Player,Time,Version>::printLinkList(){
     linkTimes();
 	std::stringstream aux;
 	head->printLinkList(aux);
     return aux.str();
 }
-
 
 #endif
